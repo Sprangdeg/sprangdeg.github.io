d1171b0ac63bbb40db39558fe6b2bcb5bb221bc7
Daniel B <d.bonstrom@gmail.com>
2023-07-26 23:15:06 +0200

    angular blog

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { NavigationEnd, Router } from '@angular/router';
+
 import { Component } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showNavMenu = true;
+
+  currentRoute = '';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
+  }
 
   toggleNavMenu() {
     this.showNavMenu = !this.showNavMenu;
