bfb6ec10e43dce7a61cec7ac1b847f5c3ae22d73
daniel <d.bonstrom@gmail.com>
2024-02-18 09:39:45 +0100

    Add async component

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { NgrxOperatorsComponent } from './posts/ngrx-operators/ngrx-operators.component';
+import { AsyncComponent } from './posts/async/async.component';
 
 const routes: Routes = [
-  { path: 'NgrxOperators', component: NgrxOperatorsComponent },
+  { path: 'ngrx-operators', component: NgrxOperatorsComponent },
+  { path: 'async', component: AsyncComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgrxOperatorsComponent } from './posts/ngrx-operators/ngrx-operators.component';
 import { CardComponent } from './components/card/card.component';
+import { AsyncComponent } from './posts/async/async.component';
 
 @NgModule({
-  declarations: [AppComponent, NgrxOperatorsComponent, CardComponent],
+  declarations: [AppComponent, NgrxOperatorsComponent, CardComponent, AsyncComponent],
   imports: [
     AppMaterialModule,
     BrowserModule,
